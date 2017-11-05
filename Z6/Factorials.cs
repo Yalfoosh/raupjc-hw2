09794b11cadc5a25daa717db0fd8db4ec633405c
Yalfoosh <suflajmob@gmail.com>
2017-11-05 17:54:59 +0100

    Fixing Z6 & Z7
    Added case check for negative factorials.

--- Z6/Factorials.cs
+++ Z6/Factorials.cs
@@ -1,21 +1,29 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace Z6
 {
     public class Factorials
     {
-        public static async Task<int> FactorialDigitSum(int n)
+        public static int FDSum(int n)
         {
-            ulong t = 1;
-            int toRet = 0;
+            if (n > 0)
+            {
+                ulong t = 1;
+                int toRet = 0;
 
-            for (int i = n; i > 1; --i)
-                t *= (ulong)i;
+                for (int i = n; i > 1; --i)
+                    t *= (ulong)i;
 
-            for (; t > 0; t /= 10)
-                toRet += (int)(t % 10);
+                for (; t > 0; t /= 10)
+                    toRet += (int)(t % 10);
 
-            return toRet;
+                return toRet;
+            }
+
+            throw new NotFiniteNumberException("The factorial of a negative integer is divergent.");
         }
+
+        public static Task<int> FactorialDigitSum(int n) => Task.Run(() => FDSum(n));
     }
 }
--- Z7/VoldemortProgram.cs
+++ Z7/VoldemortProgram.cs
@@ -12,32 +12,12 @@ namespace Z7
             Console.WriteLine(result);
         }
 
-        private static async Task<int> GetTheMagicNumber()
-        {
-            return await IKnowIGuyWhoKnowsAGuy();
-        }
+        private static async Task<int> GetTheMagicNumber() => await IKnowIGuyWhoKnowsAGuy();
+        private static async Task<int> IKnowIGuyWhoKnowsAGuy() => await IKnowWhoKnowsThis(10) + await IKnowWhoKnowsThis(5);
+        private static async Task<int> IKnowWhoKnowsThis(int n) => await Factorials.FactorialDigitSum(n);
 
-        private static async Task<int> IKnowIGuyWhoKnowsAGuy()
+        static void Main()
         {
-            return await IKnowWhoKnowsThis(10) + await IKnowWhoKnowsThis(5);
-        }
-
-        private static async Task<int> IKnowWhoKnowsThis(int n)
-        {
-            return await Factorials.FactorialDigitSum(n);
-        }
-
-        // Ignore this part .
-        static void Main(string[] args)
-        {
-            // Main method is the only method that
-            // can’t be marked with async.
-            // What we are doing here is just a way for us to simulate
-            // async - friendly environment you usually have with
-            // other. NET application types (like web apps, win apps etc.)
-            // Ignore main method, you can just focus on LetsSayUserClickedAButtonOnGuiMethod() as a
-            // first method in the call hierarchy.
-
             Task.Run(() => LetsSayUserClickedAButtonOnGuiMethod());
             Console.Read();
         }
